20b5bb37635f55c1b70d81158c4c9cf2710d26d4
Vishal Gowda <cartmanboy1991@gmail.com>
2017-04-22 16:20:15 +0530

    Memoize Davis Staircase

--- DavisStaircase.java
+++ DavisStaircase.java
@@ -1,12 +1,17 @@
 import java.util.*;
 
 public class DavisStaircase {
+    public static HashMap<Integer, Integer> ways = new HashMap<Integer, Integer>();
 
-    public static long noOfWays(int height) {
-        long ways = 0;
+    public static int noOfWays(int height) {
+        Integer w = ways.get(height);
+        if (w != null)
+            return w;
         if (height == 0) return 1;
         if (height < 0) return 0;
-        return (noOfWays(height - 1) + noOfWays(height - 2) + noOfWays(height - 3));
+        w = (noOfWays(height - 1) + noOfWays(height - 2) + noOfWays(height - 3));
+        ways.put(height, w);
+        return w;
     }
 
     public static void main (String[] args) {
